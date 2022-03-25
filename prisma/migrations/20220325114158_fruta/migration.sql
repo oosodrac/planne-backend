bbd65fbfe3c9f84b76c5f05d8a91d08c766c4007
Ramiro Cardoso <ramiro.cardoso@protonmail.com>
2022-03-25 12:46:04 +0100

    Model: criacao do model fruta

---
+++ prisma/migrations/20220325114158_fruta/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Fruta" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "preco" DECIMAL NOT NULL,
+    "expiracao" INTEGER NOT NULL
+);
