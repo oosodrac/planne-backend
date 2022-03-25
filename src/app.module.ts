d6e6891204e7f25af4b3324736ac3a1a154e326b
Ramiro Cardoso <ramiro.cardoso@protonmail.com>
2022-03-25 12:13:23 +0100

    Config: make discover of BaldeService and BaldeController

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { BaldeService } from './balde/balde.service';
+import { BaldeController } from './balde/balde.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [AppController, BaldeController],
+  providers: [AppService, PrismaService, BaldeService],
 })
 export class AppModule {}
